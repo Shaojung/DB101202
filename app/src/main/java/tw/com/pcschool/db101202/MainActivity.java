d0fe311ecbc80f532df72edee836e105acb7e7f0
Shaojung <abc@abc.com>
2016-10-12 21:38:22 +0800

    add change.

--- app/src/main/java/tw/com/pcschool/db101202/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/db101202/MainActivity.java
@@ -8,6 +8,7 @@ import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-
+                Log.d("LOC", "Change:" + location.getLatitude() + "," + location.getLongitude());
             }
 
             @Override
